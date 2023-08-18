f89a37f01fb96a3381eb658630fa3b5a45adcb11
ronaldornd <ronaldo.s.barbosa@outlook.com>
2023-08-18 16:30:17 -0300

    first basic code

---
+++ src/main/java/br/com/ifpe/oxefoodapi/OxefoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiApplication.class, args);
+	}
+
+}
