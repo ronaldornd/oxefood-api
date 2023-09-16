a61040c45c227fdcc99008fbfa6cb785bd625bcf
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2023-09-16 02:21:11 -0300

    add cliente classes

---
+++ src/main/java/br/com/ifpe/oxefoodapi/modelo/cliente/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.ifpe.oxefoodapi.modelo.cliente;
+
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+   @Autowired
+   private ClienteRepository repository;
+
+   @Transactional
+   public Cliente save(Cliente cliente) {
+
+       cliente.setHabilitado(Boolean.TRUE);
+       cliente.setVersao(1L);
+       cliente.setDataCriacao(LocalDate.now());
+       return repository.save(cliente);
+   }
+
+}
