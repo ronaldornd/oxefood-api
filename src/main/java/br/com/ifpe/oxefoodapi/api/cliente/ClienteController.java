74164c5fd0421b94eeff4f390419362230976bc2
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2023-10-27 16:34:27 -0300

    add swagger

--- src/main/java/br/com/ifpe/oxefoodapi/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefoodapi/api/cliente/ClienteController.java
@@ -18,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ifpe.oxefoodapi.modelo.cliente.Cliente;
 import br.com.ifpe.oxefoodapi.modelo.cliente.ClienteService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin
@@ -26,17 +31,28 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+    @ApiOperation(value = "Serviço responsável por salvar um cliente no sistema.")
     @PostMapping
     public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
         Cliente cliente = clienteService.save(request.build());
         return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Serviço responsável por listar todos os clientes do sistema.")
     @GetMapping
     public List<Cliente> findAll() {
         return clienteService.findAll();
     }
 
+    @ApiOperation(value = "Serviço responsável por obter um cliente referente ao Id passado na URL.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna  o cliente."),
+        @ApiResponse(code = 401, message = "Acesso não autorizado."),
+        @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso."),
+        @ApiResponse(code = 404, message = "Não foi encontrado um registro para o Id informado."),
+        @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
+    })
+ 
     @GetMapping("/{id}")
     public Cliente findById(@PathVariable Long id) {
         return clienteService.findById(id);
