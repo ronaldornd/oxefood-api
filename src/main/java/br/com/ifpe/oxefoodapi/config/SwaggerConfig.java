e3d3620324f2cda23de7d27d188985c47468a0be
unknown <@alunoifpe>
2023-11-01 14:49:47 -0300

    working fine

--- src/main/java/br/com/ifpe/oxefoodapi/config/SwaggerConfig.java
+++ src/main/java/br/com/ifpe/oxefoodapi/config/SwaggerConfig.java
@@ -1,7 +1,27 @@
 package br.com.ifpe.oxefoodapi.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -17,34 +37,64 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
-    @Bean
-    public Docket apiDocket() {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootApplication.class, args);
+  }
 
-	return new Docket(DocumentationType.SWAGGER_2)
-		.select()
-		.apis(RequestHandlerSelectors.any())
-		.paths(PathSelectors.any())
-		.build()
-		.apiInfo(metaData());
-    }
+  @Bean
 
-    private ApiInfo metaData() {
+  public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+      ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+      EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+      WebEndpointProperties webEndpointProperties, Environment environment) {
+    List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+    allEndpoints.addAll(webEndpoints);
+    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+    allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+    String basePath = webEndpointProperties.getBasePath();
+    EndpointMapping endpointMapping = new EndpointMapping(basePath);
+    boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
+        basePath);
+    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+        corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+        shouldRegisterLinksMapping, null);
+  }
 
-	return new ApiInfoBuilder()
-		.title("App OxeFood REST API")
-		.description("\"App OxeFood REST API for greeting people\"")
-		.version("1.0.0")
-		.license("Apache License Version 2.0")
-		.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-		.build();
-    }
+  private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
+      String basePath) {
+    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+        || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+  }
 
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-	
-	registry.addResourceHandler("swagger-ui.html")
-		.addResourceLocations("classpath:/META-INF/resources/");
+  public Docket apiDocket() {
 
-	registry.addResourceHandler("/webjars/**")
-		.addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(metaData());
+  }
+
+  private ApiInfo metaData() {
+
+    return new ApiInfoBuilder()
+        .title("App OxeFood REST API")
+        .description("\"App OxeFood REST API for greeting people\"")
+        .version("1.0.0")
+        .license("Apache License Version 2.0")
+        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+        .build();
+  }
+
+  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+
 }
